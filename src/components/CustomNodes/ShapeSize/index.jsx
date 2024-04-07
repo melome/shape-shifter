d535c6cf7b8c0ec339f5912d6c3e134d61fde5c4
Shanley Grace Rosero <shanleyrosero@gmail.com>
2024-04-07 21:32:09 +0800

    🐛 fix(node connection): fix issue on unconected nodes (fixed #2)
    ensured removed connection of a shape node configuration does not affect/reflect the output
    
    fixed #2

--- src/components/CustomNodes/ShapeSize/index.jsx
+++ src/components/CustomNodes/ShapeSize/index.jsx
@@ -3,12 +3,33 @@ import { Paper, Slider } from '@mui/material';
 import { Handle, Position } from 'reactflow';
 
 import useShapeStore from '../../../store/shapeStore';
+import { useStore } from '../../../store/reactflowStore';
 
 export default function ShapeSize({ data, isConnectable }) {
     const { shapeSize, setShapeSize } = useShapeStore();
+    const {nodes, connectedEdges} = useStore();
+
+    // Filter nodes with type "shapeSize"
+    const shapeColorNode = nodes.find((node) => {
+        if (node.type === "shapeSize") {
+            console.log(node);
+            return node;
+        }
+    });
+
+    const outputNodeId = "4";
+
+    const isConnected = connectedEdges.find((connection) => {
+        if (connection.source === shapeColorNode.id && connection.target === outputNodeId) {
+            return true;
+        }
+    })
+
 
     const handleShapeSize = (event, newValue) => {
-        setShapeSize(newValue);
+        if (isConnected){
+            setShapeSize(newValue);
+        }
     };
 
     return (
